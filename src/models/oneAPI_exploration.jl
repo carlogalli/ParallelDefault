2e4892f95995abf546e308c31754dec4cf5d03e0
carlogalli <carlogalli87@gmail.com>
2022-10-03 12:16:29 +0200

    finished checking all fns work

--- src/models/oneAPI_exploration.jl
+++ src/models/oneAPI_exploration.jl
@@ -3,79 +3,22 @@ if pwd() != "/home/carlo/Documents/ParallelDefault"
 end
 using Pkg; Pkg.activate(".")
 
-using Distributions, oneAPI, Printf, BenchmarkTools
+using Distributions, oneAPI, Printf, BenchmarkTools, Test
 #Initialization
 
 
 ##
-#= this is still to be digested =#
-
-#line 8 Calculate Vr, still a double loop inside, tried to flatten out another loop
-function vr(Nb,Ny,α,β,τ,Vr,V0,Y,B,Price0,P)
-
-    ib = (blockIdx().x-1)*blockDim().x + threadIdx().x
-    iy = (blockIdx().y-1)*blockDim().y + threadIdx().y
-
-    if (ib <= Nb && iy <= Ny)
-
-        Max = -Inf
-        for b in 1:Nb
-            c = exp(Y[iy]) + B[ib] - Price0[iy,b]*B[b]
-            if c > 0 #If consumption positive, calculate value of return
-                sumret = 0
-                for y in 1:Ny
-                    sumret += V0[y,b]*P[iy,y]
-                end
-
-                vr = (c^(1-α))/(1-α) + β * sumret
-                Max = max(Max, vr)
-            end
-        end
-        Vr[iy,ib] = Max
-    end
-    return
-end
-
-
-#line 9-14 debt price update
-function Decide(Nb,Ny,Vd,Vr,V,decision,decision0,prob,P,Price,rstar)
-
-    ib = (blockIdx().x-1)*blockDim().x + threadIdx().x
-    iy = (blockIdx().y-1)*blockDim().y + threadIdx().y
-
-    if (ib <= Nb && iy <= Ny)
-
-        if (Vd[iy] < Vr[iy,ib])
-            V[iy,ib] = Vr[iy,ib]
-            decision[iy,ib] = 0
-        else
-            V[iy,ib] = Vd[iy]
-            decision[iy,ib] = 1
-        end
-
-        for y in 1:Ny
-            prob[iy,ib] += P[iy,y] * decision[y,ib]
-        end
-
-        Price[iy,ib] = (1-prob[iy,ib]) / (1+rstar)
-
-    end
-    return
-end
-
-
-## -----------------------------------------------------------------------------------
-#Main starts
+#= Main starts =#
 
 
 #Setting parameters
-Ny = 100 #grid number of endowment
-Nb = 150 #grid number of bond
+Ny = Int32(100) #grid number of endowment
+Nb = Int32(150) #grid number of bond
 maxInd = Ny * Nb #total grid points
-rstar = 0.017 #r* used in price calculation
+rstar = Float32(0.017) #r* used in price calculation
 α = Float32(0.5) #α used in utility function
-lbd = -1 #lower bound and upper bound for bond initialization
-ubd = 0  #lower bound and upper bound for bond initialization
+lbd = Float32(-1) #lower bound and upper bound for bond initialization
+ubd = Float32(0)  #lower bound and upper bound for bond initialization
 β = Float32(0.953)   #β,ϕ,τ used as in part 4 of original paper
 ϕ = Float32(0.282)   #β,ϕ,τ used as in part 4 of original paper
 τ = Float32(0.5) #β,ϕ,τ used as in part 4 of original paper
@@ -132,8 +75,8 @@ Vd = oneAPI.zeros(Float32, Ny)                          #Value of default
 decision = oneAPI.ones(Float32, Ny,Nb)                  #Decision matrix
 
 
-## -----------------------------------------------------------------------------------
-# This is where the while loop starts!
+##
+#= This is where the while loop starts! =#
 
 
 #Keeping copies of Value, Value of defualt, Price for the previous round
@@ -144,99 +87,130 @@ prob = oneAPI.zeros(Float32, Ny,Nb)
 decision = oneAPI.ones(Float32, Ny,Nb)
 decision0 = oneAPI.deepcopy(decision)
 
-threadcount = 256 #set up defualt thread numbers per block
 
 
 
-
-## -----------------------------------------------------------------------------------
-# Initialise the default value
+##
+#= Initialise the default value =#
 
 sumdef = oneAPI.zeros(Float32, Ny)
-tid = oneAPI.zeros(Float32, Ny)
-bid = oneAPI.zeros(Float32, Ny)
 
 # Intitializing value of default U((1-τ)iy) to each Vd[iy]
-function def_init!(sumdef,tid,bid,τ,Y,α)
-    iy = get_global_id()       # thread index
-    stride = get_local_size()   # number of threads in a group
-    for i = iy:stride:length(sumdef)
-        sumdef[i] = (exp((1-τ)*Y[i])^(1-α))/(1-α)
-        tid[i] = get_global_id()
-        bid[i] = get_group_id()
+function def_init!(sumdef, τ, Y, α, Ny)
+    y0 = get_global_id(0)       # thread index
+    if (y0 <= Ny)
+        sumdef[y0] = (exp((1-τ)*Y[y0])^(1-α))/(1-α)
     end
     return
 end
 
-@oneapi items=Ny def_init!(sumdef,tid,bid,τ,Y,α)
+@oneapi items=10 def_init!(sumdef,τ,Y,α,Ny)
 
 
 
-## -----------------------------------------------------------------------------------
-# Use def_add
-Ny = 33
+##
+#= Use def_add =#
 
 temp = oneAPI.zeros(Float32, Ny,Ny)
 fill!(temp, NaN)
-tx, ty, gx, gy, gsx, gsy = [oneAPI.zeros(Float32, Ny,Ny) for i in 1:6]
 
 #line 7.2 adding second expected part to calcualte Vd[iy]
-function def_add!(matrix, P, β, V0, Vd0, ϕ, Ny, tx, ty, gx, gy, gsx, gsy)
-    gid_x = get_global_id(0)
-    gid_y = get_global_id(1)
-    tid_x = get_local_id(0)
-    tid_y = get_local_id(1)
-    bid_x = get_group_id(0)
-    bid_y = get_group_id(1)
-    bdim_x = get_local_size(0)
-    bdim_y = get_local_size(1)
-
-    y0 = (bid_x-1)*bdim_x + tid_x
-    y1 = (bid_y-1)*bdim_y + tid_y
-
-    oneAPI.@println("tid_x=$tid_x, tid_y=$tid_y, bid_x=$bid_x, bid_y=$bid_y, gid_x=$gid_x, gid_y=$gid_y, y0=$y0, y1=$y1")
+function def_add!(matrix, P, β, V0, Vd0, ϕ, Ny)
+    y0 = get_global_id(0)
+    y1 = get_global_id(1)
     
     if (y0 <= Ny && y1 <= Ny)
-        # matrix[y0,y1] = β* P[y0,y1]* (ϕ* V0[y1,1] + (1-ϕ)* Vd0[y1])
-        matrix[y0,y1] = 3.
-        # tx[y0,y1] = tid_x
-        # ty[y0,y1] = tid_y
-        # gx[y0,y1] = bid_x
-        # gy[y0,y1] = bid_y
-        # gsx[y0,y1] = bdim_x
-        # gsy[y0,y1] = bdim_y
+        matrix[y0,y1] = β* P[y0,y1]* (ϕ* V0[y1,1] + (1-ϕ)* Vd0[y1])
     end
     return
 end
 
-# nt = 2^4
-nt = 4
+nt = 2^4
 threadcount = (nt, nt)
 nb = cld(Ny, nt)
-nb = 2
 blockcount = (nb, nb)
 
-@oneapi items=threadcount groups=blockcount def_add!(temp, P, β, V0, Vd0, ϕ, Ny, tx, ty, gx, gy, gsx, gsy)
+@oneapi items=threadcount groups=blockcount def_add!(temp, P, β, V0, Vd0, ϕ, Ny)
 temp
+
+@test all(isnan.(temp).==false)
+
+# Added this part for speed, may not work so well and untidy
+temp = sum(temp, dims=2)
+
+
+
+##
+#= Compute Repay =#
+
+function vr(Nb,Ny,α,β,Vr,V0,Y,B,Price0,P)
+
+    # ib = (blockIdx().x-1)*blockDim().x + threadIdx().x
+    # iy = (blockIdx().y-1)*blockDim().y + threadIdx().y
+    ib0 = get_global_id(0)
+    iy0 = get_global_id(1)
+    
+    oneAPI.@println("ib0 $ib0 iy0 $iy0")
+
+    if (ib0 <= Nb && iy0 <= Ny)        
+
+        Max = Float32(-Inf)
+        for ib1 in 1:Nb
+            c = exp(Y[iy0]) + B[ib0] - Price0[iy0,ib1]*B[ib1]            
+            if c > 0 #If consumption positive, calculate value of return
+                sumret = 0
+                for iy1 in 1:Ny
+                    sumret += V0[iy1,ib1]*P[iy0,iy1]
+                end
+
+                vr = (c^(1-α))/(1-α) + β * sumret
+                Max = max(Max, vr)
+            end
+        end
+        Vr[iy0,ib0] = Max
+    end
+    return
+end
+
+nt = 2^4
+threadcount = (nt, nt)
+blockcount = (cld(Nb, nt), cld(Ny, nt))
+@oneapi items=threadcount groups=blockcount vr(Nb,Ny,α,β,Vr,V0,Y,B,Price0,P)
+Vr
 
 ##
 
+function Decide(Nb,Ny,Vd,Vr,V,decision,decision0,prob,P,Price,rstar)
 
-#= check this stuff! =#
+    # ib = (blockIdx().x-1)*blockDim().x + threadIdx().x
+    # iy = (blockIdx().y-1)*blockDim().y + threadIdx().y
+    ib0 = get_global_id(0)
+    iy0 = get_global_id(1)
 
-#Added this part for speed, may not work so well and untidy
-temp = sum(temp, dims=2)
-Vd = sumdef + temp
+    if (ib0 <= Nb && iy0 <= Ny)
 
-#line 8
+        if (Vd[iy0] < Vr[iy0,ib0])
+            V[iy0,ib0] = Vr[iy0,ib0]
+            decision[iy0,ib0] = Int32(0)
+        else
+            V[iy0,ib0] = Vd[iy0]
+            decision[iy0,ib0] = Int32(1)
+        end
 
-# blockcount = (ceil(Int,Nb/10),ceil(Int,Ny/10))
-@cuda threads=threadcount blocks=blockcount vr(Nb,Ny,α,β,τ,Vr,V0,Y,B,Price0,P)
+        for iy1 in 1:Ny
+            prob[iy0,ib0] += P[iy0,iy1] * decision[iy1,ib0]
+        end
 
-#line 9-14
+        Price[iy0,ib0] = (1-prob[iy0,ib0]) / (1+rstar)
 
-# blockcount = (ceil(Int,Nb/10),ceil(Int,Ny/10))
-@cuda threads=threadcount blocks=blockcount Decide(Nb,Ny,Vd,Vr,V,decision,decision0,prob,P,Price,rstar)
+    end
+    return
+end
+
+nt = 2^4
+threadcount = (nt, nt)
+blockcount = (cld(Nb, nt), cld(Ny, nt))
+@oneapi items=threadcount groups=blockcount Decide(Nb,Ny,Vd,Vr,V,decision,decision0,prob,P,Price,rstar)
 
 #line 16
 #update Error and value matrix at round end
